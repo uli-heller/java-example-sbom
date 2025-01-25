0d706b48990927aff3eaf08ebf9b1fac243f4049
Uli Heller <uli.heller@daemons-point.com>
2025-01-25 12:05:28 +0100

    Added content of maven repository

--- create-version.sh
+++ create-version.sh
@@ -5,6 +5,26 @@
 # ./create-version.sh hello-world 1.0.0
 # -> creates for "hello-world" the version within the local maven repository
 #
+#      maven-repository/cool/heller/uli/hello-world/maven-metadata.xml
+#      maven-repository/cool/heller/uli/hello-world/maven-metadata.xml.sha512
+#      maven-repository/cool/heller/uli/hello-world/maven-metadata.xml.sha1
+#      maven-repository/cool/heller/uli/hello-world/maven-metadata.xml.sha256
+#      maven-repository/cool/heller/uli/hello-world/maven-metadata.xml.md5
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0.module.sha512
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0-plain.jar
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0.module.sha256
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0.pom
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0-plain.jar.md5
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0-plain.jar.sha512
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0.module.sha1
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0.pom.sha256
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0-plain.jar.sha1
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0-plain.jar.sha256
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0.pom.md5
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0.module
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0.pom.sha1
+#      maven-repository/cool/heller/uli/hello-world/1.0.0/hello-world-1.0.0.module.md5
+#
 # ./create-version.sh bye-moon  0.9.0 0.9.1 1.0-SNAPSHOT
 # -> creates for "bye-moon" multiple versions within the local maven repository
 #

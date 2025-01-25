0d07207a17b238f2e758a0abc1be9be067aa8db2
Uli Heller <uli.heller@daemons-point.com>
2025-01-25 13:33:33 +0100

    Removed obsolete lines

--- create-maven-repository.sh
+++ create-maven-repository.sh
@@ -6,8 +6,6 @@ D="$(realpath "${D}")"
 MAVEN_REPOSITORY="${D}/maven-repository"
 MAVEN_REPOSITORY_URL="file://${MAVEN_REPOSITORY}"
 
-#rm -rf "${MAVEN_REPOSITORY}"
-
 rm -rf "${MAVEN_REPOSITORY}"
 "${D}/create-version.sh" hello-world 0.8.0 0.9.0 0.9.1
 "${D}/create-version.sh" bye-moon    0.8.0 0.9.0 1.0-SNAPSHOT
